bf3cda2a325f79d3c5d6c2553ddd04445f21354c
antisch <antisch@microsoft.com>
2025-02-13 08:21:00 +1300

    Test restructure

--- test/test_provision.py
+++ test/test_provision.py
@@ -15,49 +15,6 @@ from azure.cloudmachine.resources.ai.deployment import AIDeployment, AIChat, AIE
 TEST_SUB = '6e441d6a-23ce-4450-a4a6-78f8d4f45ce9'
 
 
-def _get_infra_dir() -> str:
-    test_dir = os.path.dirname(os.path.realpath(__file__))
-    return os.path.join(test_dir, "test_infra")
-
-
-def _compare_outputs(output_dir, ref_dir, test_dir):
-    for _, _, files in os.walk(os.path.join(output_dir, ref_dir)):
-        for filename in files:
-            with open(os.path.join(output_dir, ref_dir, filename), 'r') as _ref:
-                ref_file = _ref.readlines()
-            with open(os.path.join(output_dir, test_dir, filename), 'r') as _test:
-                test_file = _test.readlines()
-            diff = difflib.unified_diff(
-                ref_file,
-                test_file,
-                fromfile=f'{ref_dir}/{filename}',
-                tofile=f'{test_dir}/{filename}',
-            )
-            changes = ("".join(diff))
-            has_changes = bool(changes)
-            assert not has_changes, "\n" + changes
-            os.remove(os.path.join(output_dir, test_dir, filename))
-    os.rmdir(os.path.join(output_dir, test_dir))
-
-
-@pytest.fixture
-def _compare_exports(request):
-    output_dir = _get_infra_dir()
-    ref_dir = request.node.name
-    test_dir = f"infra_{request.node.name}"
-    yield output_dir, ref_dir, test_dir
-    _compare_outputs(output_dir, ref_dir, test_dir)
-
-
-@pytest.fixture
-def _record_exports(request):
-    output_dir = _get_infra_dir()
-    ref_dir = request.node.name
-    return output_dir, ref_dir, ref_dir
-
-export_dir = _record_exports if os.environ.get('TEST_EXPORT_RECORD', "").lower() == 'true' else _compare_exports
-
-
 def test_export_resourcegroup(export_dir):
     r = ResourceGroup()
     export(r, output_dir=export_dir[0], infra_dir=export_dir[2], name="test")
